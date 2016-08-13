2d8394f22c895593eadf52526c7855d8028f59d6
Pascal Cremer <b00gizm@gmail.com>
2016-08-14 01:22:42 +0200

    Add request handlers via "callbacks"

--- alexa.go
+++ alexa.go
@@ -4,6 +4,10 @@ import (
 	"encoding/json"
 )
 
+func Default() *Alexa {
+	return Init([]Handler{})
+}
+
 func Init(handlers []Handler) *Alexa {
 	var launchHandler LaunchHandler
 	var sessionEndedHandler SessionEndedHandler
@@ -91,6 +95,20 @@ func (a *Alexa) Process(msg json.RawMessage) (*Response, error) {
 	}
 
 	return newEmptyResponse(), nil
+}
+
+func (a *Alexa) OnLaunch(handler func(a *Alexa, req *Request, session *Session) *Response) {
+	a.launchHandler = LaunchHandlerFunc(handler)
+}
+
+func (a *Alexa) OnIntent(handler func(a *Alexa, intent *Intent, req *Request, session *Session) *Response) {
+	a.intentHandlers = []IntentHandler{
+		IntentHandlerFunc(handler),
+	}
+}
+
+func (a *Alexa) OnSessionEnded(handler func(a *Alexa, req *Request, session *Session) *Response) {
+	a.sessionEndedHandler = SessionEndedHandlerFunc(handler)
 }
 
 func (a *Alexa) Response() *Response {
--- handler.go
+++ handler.go
@@ -16,3 +16,21 @@ type SessionEndedHandler interface {
 	Handler
 	HandleSessionEnded(a *Alexa, req *Request, session *Session) *Response
 }
+
+type LaunchHandlerFunc func(a *Alexa, req *Request, session *Session) *Response
+
+func (fn LaunchHandlerFunc) HandleLaunch(a *Alexa, req *Request, session *Session) *Response {
+	return fn(a, req, session)
+}
+
+type IntentHandlerFunc func(a *Alexa, intent *Intent, req *Request, session *Session) *Response
+
+func (fn IntentHandlerFunc) HandleIntent(a *Alexa, intent *Intent, req *Request, session *Session) *Response {
+	return fn(a, intent, req, session)
+}
+
+type SessionEndedHandlerFunc func(a *Alexa, req *Request, session *Session) *Response
+
+func (fn SessionEndedHandlerFunc) HandleSessionEnded(a *Alexa, req *Request, session *Session) *Response {
+	return fn(a, req, session)
+}
